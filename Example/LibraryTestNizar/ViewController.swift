b59ddf6301643d80a8744b5c20f5679db1a24834
Sharulnizar <sharulnizar.razali@gmail.com>
2022-11-12 21:03:39 +0800

    add sample code in example

--- Example/LibraryTestNizar/ViewController.swift
+++ Example/LibraryTestNizar/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import LibraryTestNizar
 
 class ViewController: UIViewController {
 
+    let scrollView = DScrollView()
+    let scrollViewContainer = DScrollViewContainer(axis: .vertical, spacing: 10)
+    let scrollViewElement0 = DScrollViewElement(height: 1200, backgroundColor: .purple)
+    let scrollViewElement1 = DScrollViewElement(height: 500, backgroundColor: .purple)//.withBackground(image: #imageLiteral(resourceName: "icon.png"), contentMode: .scaleAspectFit)
+    
+    let label = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .white
+        label.text = "I am a label"
+        
+        view.addScrollView(scrollView,
+                           withSafeArea: .none,
+                           hasStatusBarCover: true,
+                           statusBarBackgroundColor: .white,
+                           container: scrollViewContainer,
+                           elements: scrollViewElement0, scrollViewElement1)
+        
+        scrollViewElement1.addSubview(label)
+        label.edgeTo(scrollViewElement1)
     }
 
     override func didReceiveMemoryWarning() {
